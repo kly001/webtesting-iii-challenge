fec70c3204afc6c56d542a1b9f87715520ce696e
kly001 <karenly1961@gmail.com>
2019-10-02 21:54:49 -0400

    Setup imports for Dashboard, Display, Controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from "react";
+import {render, fireEvent} from "@testing-library/react";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+
+import Controls from "./Controls"
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,7 @@
 // Test away!
+import React from "react";
+import {render, fireEvent} from "@testing-library/react";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display";
